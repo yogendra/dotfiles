05f70b411bc81a08be16c0bcb2934300a530eb98
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2020-02-25 21:35:30 +0800

    updated to use new bosh commands

--- scripts/pcf-stop-start.sh
+++ scripts/pcf-stop-start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-if [ $1 == "shut" -o $1 == "start" ];
-        then
-                echo "Running PCF $1 Process..."
-        else
-                echo "Only shut or start are valid args!"
-                exit 1
+if [ "$1" == "shut" -o "$1" == "start" ]
+then
+        echo "Running PCF $1 Process..."
+else
+        echo "Only shut or start are valid args!"
+        exit 1
 fi
 
 
@@ -19,26 +19,16 @@ cloud_controller_worker
 
 
 if [ $1 == "shut" ]; then
- jobVMs=$(bosh vms --detail|grep partition| awk -F '|' '{ print $2 }')
- bosh vm resurrection off
- for (( i=${#bootOrder[@]}-1; i>=0; i-- )); do
-        for x in $jobVMs; do
-                jobId=$(echo $x | awk -F "/" '{ print $1 }')
-                instanceId=$(echo $x | awk -F "/" '{ print $2 }')
-                jobType=$(echo $jobId | awk -F "-" '{ print $1 }')
-                        if [ "$jobType" == "${bootOrder[$i]}" ];
-                        then
-                                #echo MATCHVAL---${bootOrder[$i]} JOBTYPE----$jobType JOBID----$jobId Instance-------$instanceId
-                                bosh -n stop $jobId --hard
-                        fi
-        done;
-
- done
- bosh -n stop --hard
-fi
-
-
-if [ $1 == "start" ]; then
- bosh -n start
- bosh vm resurrection on
+        bosh deployments --json | jq ".Tables[0].Rows[].name" -r | while read deployment 
+        do 
+                echo $deployment: Stopping
+                bosh stop -d $deployment --hard --skip-drain --non-interactive
+        done
+elif [ $1 == "start" ]; then
+        bosh deployments --json | jq ".Tables[0].Rows[].name" -r | while read deployment 
+        do 
+                echo $deployment: Starting
+                bosh -d $deployment  start  --non-interactive
+        done
+        bosh update-resurrection on
 fi
