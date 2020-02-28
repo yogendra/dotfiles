9ab81aea9cf724d516cad2376f3525a520123cd5
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2020-02-28 23:14:31 +0800

    Update om install script to allow latest version install

--- scripts/om-install.sh
+++ scripts/om-install.sh
@@ -2,25 +2,37 @@
 # From an installation project folder (configured to connect to pcf/opsmanager), run following to upload tiles 
 # wget -qO- "https://gist.github.com/yogendra/318c09f0cd2548bdd07f592722c9bbec/raw/om-install.sh"  | bash
 
+PCF_IAAS=${PCF_IAAS:-google}
+TILE_DIR=${TILE_DIR:-$PWD}
+download_file=$TILE_DIR/download-file.json
 
-function pipe(){
+function om-install(){
     product=$1
-    version=$2
-    fileglob=$3
-    ufileglob=$(echo $fileglob | sed 's/\\//')
-
-    echo om download-product --pivnet-api-token $PIVNET_TOKEN  --product $product --product-version $version --pivnet-file-glob $fileglob
-    echo om -k upload-product -p $ufileglob
+    fileglob=$2
+    version=${3:-`pivnet rs -p $product -l 1 -o json | jq -r '.[0].version'`}
+    echo "Download $product :: $version ($fileglob)"
+    om download-product --pivnet-api-token $OM_PIVNET_TOKEN  --product $product --product-version $version --pivnet-file-glob $fileglob --download-stemcell --stemcell-iaas=$PCF_IAAS
+    tile_path=$(jq -r '.product_path' $download_file)
+    tile_version=$(jq -r '.product_version' $download_file)
+    tile_slug=$(jq -r '.product_slug' $download_file)
+    om -k upload-product -p $tile_path  
 }
-pipe apm '1.6.0'  "apm\*.pivotal"
-pipe credhub-service-broker '1.2.0'  "credhub-service-broker\*.pivotal"
-pipe p-cloud-cache '1.6.1'  "p-cloud-cache\*.pivotal"
-pipe p-dataflow '1.3.1'  "p-dataflow\*.pivotal"
-pipe p-event-alerts '1.2.6'  "p-event-alerts\*.pivotal"
-pipe p-healthwatch '1.4.4'  "p-healthwatch\*.pivotal"
-pipe p-rabbitmq '1.15.3'  "p-rabbitmq\*.pivotal"
-pipe p-redis '2.0.0'  "p-redis-\*.pivotal"
-pipe p-scheduler '1.1.4'  "p-scheduler\*.pivotal"
-pipe p-spring-cloud-services '2.0.5'  "p-spring-cloud-services-\*.pivotal"
-pipe pivotal-mysql '2.5.3'  "pivotal-mysql\*.pivotal"
-pipe pivotal_single_sign-on_service 1.7.2  "Pivotal_Single_Sign-On_Service\*.pivotal"
+if [[ $# -lt 2 ]]; then
+    cat <<EOF
+om-install product-slug filename [version]
+EOF
+    exit 1
+fi
+om-install $*
+# pipe apm "apm\*.pivotal"
+# pipe credhub-service-broker "credhub-service-broker\*.pivotal"
+# pipe p-cloud-cache "p-cloud-cache\*.pivotal"
+# pipe p-dataflow "p-dataflow\*.pivotal"
+# pipe p-event-alerts "p-event-alerts\*.pivotal"
+# pipe p-healthwatch "p-healthwatch\*.pivotal"
+# pipe p-rabbitmq "p-rabbitmq\*.pivotal"
+# pipe p-redis "p-redis-\*.pivotal"
+# pipe p-scheduler "p-scheduler\*.pivotal"
+# pipe p-spring-cloud-services "p-spring-cloud-services-\*.pivotal"
+# pipe pivotal-mysql "pivotal-mysql\*.pivotal"
+# pipe pivotal_single_sign-on_service "Pivotal_Single_Sign-On_Service\*.pivotal"
