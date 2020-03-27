f63a7ecc807666963961dbad3d739ccecee82b3b
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2020-03-27 20:12:01 +0800

    Added support for fixed github version and pivnet version

--- scripts/generate-versions.sh
+++ scripts/generate-versions.sh
@@ -13,16 +13,23 @@ function add_version {
 }
 function pivnet_version {
     product=$1
-    version="$(pivnet releases -p $product --format json | jq -r '. |= sort_by("release_date") | .[0].version' | sed 's/^v//')"
+    version=$2
+    if [[ -z $version ]]; then
+        version="$(pivnet releases -p $product --format json |jq -r '[.[] | select(.release_type != "Release Candidate")][0].version' | sed 's/^v//')"
+    fi
     add_version "$product" "$version"
 }
 
 function github_version {
     product=$1
     repo=$2
-    version="$(wget ${GITHUB_OPTIONS} -qO- https://api.github.com/repos/$repo/releases  | jq -r '. | map(select (.prerelease == false))| .[0].tag_name' | sed 's/^v//')"
-    [[ $version == "null" ]] && \
-        version="$(wget ${GITHUB_OPTIONS} -qO- https://api.github.com/repos/$repo/releases  | jq -r '.[0].tag_name' | sed 's/^v//')"
+    version=$3
+    if [[ -z $version ]]; then
+
+        version="$(wget ${GITHUB_OPTIONS} -qO- https://api.github.com/repos/$repo/releases  | jq -r '. | map(select (.prerelease == false))| .[0].tag_name' | sed 's/^v//')"
+        [[ $version == "null" ]] && \
+            version="$(wget ${GITHUB_OPTIONS} -qO- https://api.github.com/repos/$repo/releases  | jq -r '.[0].tag_name' | sed 's/^v//')"
+    fi
     add_version "$product" "$version"
 }
 
@@ -34,6 +41,7 @@ pivnet_version "pivotal-container-service"
 pivnet_version "p-scheduler"
 pivnet_version "pcf-app-autoscaler" 
 pivnet_version "p-event-alerts"
+pivnet_version "container-services-manager"
 pivnet_version "container-services-manager"
 pivnet_version "build-service"
 
