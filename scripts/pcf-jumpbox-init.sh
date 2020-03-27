96739f10022659984b3db68f8fa4d66cbef62f67
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2020-03-28 03:27:24 +0800

    updated to use assets json

--- scripts/pcf-jumpbox-init.sh
+++ scripts/pcf-jumpbox-init.sh
@@ -2,7 +2,6 @@
 # Set these Environment variables
 #  PROJ_DIR            : Project Directory. All tools will get install under PROJ_DIR/bin. (default: $HOME)
 #  PIVNET_LEGACY_TOKEN : Pivotal Network Token (required) Its **NOT** ending with -r. It looks like DJHASLD7_HSDHA7 (default: none)
-#  GITHUB_TOKEN        : (Optional) Provide github token for accessing API. (default: none)
 #  TIMEZONE            : Timezone of the host (default:Asua/Singapore)
 #  GIT_REPO            : Git repository to use for supporting items (default: yogendra/dotfiles)
 #  DOTFILES_DIR        : Location to put dotfiles (default: $HOME/code/dotfiles)
@@ -19,8 +18,6 @@ export PATH=${PATH}:${PROJ_DIR}/bin
 PIVNET_LEGACY_TOKEN=${PIVNET_LEGACY_TOKEN}
 [[ -z ${PIVNET_LEGACY_TOKEN} ]] && echo "PIVNET_LEGACY_TOKEN environment variable not set. See instructions at https://github.com/yogendra/dotfiles/blob/master/README-PCF-TOOLS.md" && exit 1
 echo PROJ_DIR=${PROJ_DIR}
-GITHUB_OPTIONS=""
-[[ -n $GITHUB_TOKEN ]] && GITHUB_OPTIONS=" --header 'authorization: Bearer $GITHUB_TOKEN'"
 
 
 [[ -d ${PROJ_DIR}/bin ]]  || mkdir -p ${PROJ_DIR}/bin
@@ -29,6 +26,9 @@ DOTFILES_DIR=${DOTFILES_DIR:-$HOME/code/dotfiles}
 TIMEZONE=${TIMEZONE:-Asia/Singapore}
 
 sudo ln -fs /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 
 echo Install basic tools for the jumpbox
 OS_TOOLS=(\
@@ -74,31 +74,26 @@ sudo apt update && sudo apt install -qqy "${OS_TOOLS[@]}"
 
 wget -qO- "https://raw.githubusercontent.com/${GIT_REPO}/master/scripts/dotfiles-init.sh?$RANDOM"| bash
 
-VERSION_JSON=$(cat ${DOTFILES_DIR}/config/versions.json)
+asset_json=$(cat ${DOTFILES_DIR}/config/assets.json)
 function asset_version {
-  ASSET_NAME=$1
-  echo ${VERSION_JSON} | jq -r ".[\"$ASSET_NAME\"]"
+  name=$1
+  echo ${asset_json} | jq -r ".[\"$name\"].version"
+}
+function asset_url {
+  name=$1
+  echo ${asset_json} | jq -r ".[\"$name\"].url"
 }
 
-function github_asset {
-    REPO=$1
-    EXPRESSION="${2:-linux}"
-    TAG="${3:-latest}"
-    ASSET_URL="https://api.github.com/repos/${REPO}/releases/${TAG}"
-    JQ_EXPR=".assets[] | select(.name|test(\"$EXPRESSION\"))|.browser_download_url"
-    wget ${GITHUB_OPTIONS} -qO- ${ASSET_URL} | jq -r "${JQ_EXPR}"
-}
 set -e
 
 # Get updated url at https://github.com/cloudfoundry/bosh-cli/releases/latest
-URL="$(github_asset cloudfoundry/bosh-cli linux-amd64)"
+URL="$(asset_url bosh)"
 echo Downloading: bosh from ${URL}
 wget -q ${URL} -O ${PROJ_DIR}/bin/bosh
 chmod a+x ${PROJ_DIR}/bin/bosh
 
 # Get updated url at https://www.terraform.io/downloads.html
-VERSION=$(asset_version terraform)
-URL="https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip"
+URL="$(asset_url terraform)"
 echo Downloading: terraform from ${URL}
 wget -q ${URL} -O /tmp/terraform.zip
 gunzip -S .zip /tmp/terraform.zip
@@ -106,40 +101,39 @@ mv /tmp/terraform ${PROJ_DIR}/bin/terraform
 chmod a+x ${PROJ_DIR}/bin/terraform
 
 # Get updated url at https://github.com/cloudfoundry/bosh-bootloader/releases/latest
-URL="$(github_asset cloudfoundry/bosh-bootloader linux_x86-64)"
+URL="$(asset_url bbl)"
 echo Downloading: bbl from ${URL}
 wget -q ${URL} -O ${PROJ_DIR}/bin/bbl
 chmod a+x ${PROJ_DIR}/bin/bbl
 
 
 # Get updated url at https://github.com/concourse/concourse/releases/latest
-URL="$(github_asset concourse/concourse fly.\*linux-amd64.tgz\$)"
+URL="$(asset_url fly)"
 echo Downloading: fly from ${URL}
 wget -q ${URL} -O- | tar -C ${PROJ_DIR}/bin -zx fly
 chmod a+x ${PROJ_DIR}/bin/fly
 
 # Get updated url at https://github.com/pivotal-cf/om/releases/latest
-URL="$(github_asset pivotal-cf/om om-linux.\*tar.gz\$)"
+URL="$(asset_url om)"
 echo Downloading: om from ${URL}
 wget -q ${URL} -O- | tar -C ${PROJ_DIR}/bin -zx om
 chmod a+x ${PROJ_DIR}/bin/om
 
 # Get updated url at https://github.com/cloudfoundry-incubator/bosh-backup-and-restore/releases/latest
-URL="$(github_asset cloudfoundry-incubator/bosh-backup-and-restore bbr-.\*-linux-amd64\$)"
+URL="$(asset_url bbr)"
 echo Downloading: bbr from ${URL}
 wget -q ${URL} -O ${PROJ_DIR}/bin/bbr
 chmod a+x ${PROJ_DIR}/bin/bbr
 
 # Always updated version :D
 # Get updated url at https://github.com/cloudfoundry/cli/releases/latest
-VERSION=$(asset_version cf)
-URL="https://packages.cloudfoundry.org/stable?release=linux64-binary&version=${VERSION}&source=github-rel"
+URL="$(asset_url cf)"
 echo Downloading: cf from ${URL}
 wget -q ${URL}  -O- | tar -C ${PROJ_DIR}/bin -zx cf
 chmod a+x ${PROJ_DIR}/bin/cf
 
 # Get updated url at https://github.com/cloudfoundry-incubator/credhub-cli/releases/latest
-URL="$(github_asset cloudfoundry-incubator/credhub-cli credhub-linux-.\*.tgz\$)"
+URL="$(asset_url credhub)"
 echo Downloading: credhub from ${URL}
 wget -q ${URL} -O- | tar -C ${PROJ_DIR}/bin -xz  ./credhub
 chmod a+x ${PROJ_DIR}/bin/credhub
@@ -147,27 +141,26 @@ chmod a+x ${PROJ_DIR}/bin/credhub
 
 # Always updated version :D
 # Get updated url at https://storage.googleapis.com/kubernetes-release/release/stable.txt
-URL="https://storage.googleapis.com/kubernetes-release/release/$(wget -q -O - https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+URL="$(asset_url kubectl)"
 echo Downloading: kubectl from ${URL}
 wget -q ${URL} -O ${PROJ_DIR}/bin/kubectl
 chmod a+x ${PROJ_DIR}/bin/kubectl
 
 # Get updated url at https://github.com/buildpacks/pack/releases/latest
-URL="$(github_asset  buildpacks/pack pack-v.\*-linux.tgz\$)"
+URL="$(asset_url pack)"
 echo Downloading: pack from ${URL}
 wget -q ${URL} -O- | tar -C ${PROJ_DIR}/bin -zx pack
 chmod a+x ${PROJ_DIR}/bin/pack
 
 
 # Get updated url at "https://github.com/pivotal-cf/texplate/releases/latest
-URL="$(github_asset  pivotal-cf/texplate linux_amd64)"
+URL="$(asset_url texplate)"
 echo Downloading: texplate from ${URL}
 wget -q ${URL} -O ${PROJ_DIR}/bin/texplate
 chmod a+x ${PROJ_DIR}/bin/texplate
 
 # Get updated url at https://download.docker.com/linux/static/stable/x86_64/
-VERSION=$(asset_version docker)
-URL="https://download.docker.com/linux/static/stable/x86_64/docker-${VERSION}.tgz"
+URL="$(asset_url docker)"
 echo Downloading: docker from ${URL}
 wget -q ${URL} -O- | tar -C /tmp -xz  docker/docker
 mv /tmp/docker/docker ${PROJ_DIR}/bin/docker
@@ -175,33 +168,32 @@ chmod a+x ${PROJ_DIR}/bin/docker
 rm -rf /tmp/docker
 
 # Get updated url at https://github.com/docker/machine/releases/latest
-URL="$(github_asset docker/machine $(uname -s)-$(uname -m))"
+URL="$(asset_url docker-machine)"
 echo Downloading: docker-machine from ${URL}
 wget -q ${URL}  -O ${PROJ_DIR}/bin/docker-machine
 chmod a+x ${PROJ_DIR}/bin/docker-machine
 
 # Get updated url at https://github.com/docker/compose/releases/latest
-URL="$(github_asset docker/compose $(uname -s)-$(uname -m)\$)"
+URL="$(asset_url docker-compose)"
 echo Downloading: docker-compose from ${URL}
 wget -q ${URL} -O ${PROJ_DIR}/bin/docker-compose
 chmod a+x ${PROJ_DIR}/bin/docker-compose
 
 # Get updated url at https://github.com/projectriff/cli/releases/latest
-VERSION=$(asset_version riff)
-URL="$(github_asset projectriff/cli  linux-amd64 tags/${VERSION})"
+URL="$(asset_url riff)"
 echo Downloading: riff from ${URL}
 wget -q ${URL} -O- | tar -C ${PROJ_DIR}/bin -xz ./riff
 chmod a+x ${PROJ_DIR}/bin/riff
 
 # Get updated url at https://github.com/cloudfoundry-incubator/uaa-cli/releases/latest
-URL="$(github_asset cloudfoundry-incubator/uaa-cli linux-amd64)"
+URL="$(asset_url uaa)"
 echo Downloading: uaa from ${URL}
 wget -q ${URL} -O ${PROJ_DIR}/bin/uaa
 chmod a+x ${PROJ_DIR}/bin/uaa
 
 
 # Get updated url at https://github.com/pivotal-cf/pivnet-cli/releases/latest
-URL="$(github_asset pivotal-cf/pivnet-cli linux-amd64)"
+URL="$(asset_url pivnet)"
 echo Downloading: pivnet from ${URL}
 wget -q ${URL} -O ${PROJ_DIR}/bin/pivnet
 chmod a+x ${PROJ_DIR}/bin/pivnet
@@ -235,16 +227,9 @@ om download-product -t "${PIVNET_LEGACY_TOKEN}" -o /tmp -v "${VERSION}"  -p buil
 mv /tmp/duffle-${VERSION}-linux ${PROJ_DIR}/bin/pb-duffle
 chmod a+x ${PROJ_DIR}/bin/pb-duffle
 
-# Get updated url at https://github.com/projectriff/riff/releases/latest
-VERSION=$(asset_version riff)
-URL="$(github_asset projectriff/cli  linux-amd64 tags/${VERSION})"
-echo Downloading: riff from ${URL} ${VERSION}
-wget -q ${URL} -O- | tar -C ${PROJ_DIR}/bin -xz ./riff
-chmod a+x ${PROJ_DIR}/bin/riff
-
 # Get updated url at https://github.com/sharkdp/bat/releases/latest
-VERSION=$(asset_version bat)
-URL="$(github_asset  sharkdp/bat x86_64-unknown-linux-gnu tags/${VERSION})"
+VERSION="$(asset_version bat)"
+URL="$(asset_url bat)"
 echo Downloading: bat from ${URL}
 wget -q  ${URL} -O- | tar -C /tmp -xz bat-${VERSION}-x86_64-unknown-linux-gnu/bat
 mv /tmp/bat-${VERSION}-x86_64-unknown-linux-gnu/bat ${PROJ_DIR}/bin/bat
@@ -253,7 +238,7 @@ rm -rf /tmp/bat-${VERSION}-x86_64-unknown-linux-gnu
 
 
 # Get updated url at https://github.com/direnv/direnv/releases/latest
-URL="$(github_asset direnv/direnv linux-amd64)"
+URL="$(github_url direnv/direnv)"
 echo Downloading: direnv from ${URL}
 wget -q  ${URL} -O ${PROJ_DIR}/bin/direnv
 chmod a+x ${PROJ_DIR}/bin/direnv
