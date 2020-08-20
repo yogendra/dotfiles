4df1a42d8dd77d91979245bf741329431a27412e
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2020-08-20 12:22:56 +0800

    updated for env consistency

--- scripts/dotfiles-init.Linux.sh
+++ scripts/dotfiles-init.Linux.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
 
-#install 
-# fzf
-# starship
-alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
 sudo apt-get update 
-sudo apt-get install -qqy git wget fzf direnv
+sudo apt-get install -qqy git wget direnv 
 git clone --bare --git-dir $HOME/.dotfiles.git  https://github.com/${GIT_REPO}.git 
-dotfiles checkout
+
+/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME checkout
+
+curl -fsSL https://starship.rs/install.sh | bash
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME checkout
+
+source $HOME/.bashrc
 
