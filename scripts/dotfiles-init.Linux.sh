303afc92c1508b23dde8fbdb7727a693720fce04
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2020-08-20 11:56:01 +0800

    simplified apt-get

--- scripts/dotfiles-init.Linux.sh
+++ scripts/dotfiles-init.Linux.sh
@@ -6,10 +6,7 @@
 # starship
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
 sudo apt-get update 
-sudo apt-get install -qqy git
+sudo apt-get install -qqy git wget fzf direnv
 git clone --bare --git-dir $HOME/.dotfiles.git  https://github.com/${GIT_REPO}.git 
 dotfiles checkout
-
-
-apt-get install curl wget fzf direnv
 
