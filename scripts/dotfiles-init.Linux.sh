b3b406e04e11c757dbe41b180de32402ed20fdbd
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2020-08-20 12:42:04 +0800

    Updated init

--- scripts/dotfiles-init.Linux.sh
+++ scripts/dotfiles-init.Linux.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-
+mkdir -p $HOME/bin
 sudo apt-get update 
 sudo apt-get install -qqy git wget direnv 
 [[  -d $HOME/.dotfiles.git ]] ||  git clone --bare --git-dir $HOME/.dotfiles.git  https://github.com/${GIT_REPO}.git 
 
 /usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME checkout
 
-curl -fsSL https://starship.rs/install.sh | bash
+curl -fsSL https://starship.rs/install.sh -o /tmp/starship-install.sh
+bash /tmp/starship-install.sh -b $HOME/bin  -y
+rm /tmp/starship-install.sh
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
