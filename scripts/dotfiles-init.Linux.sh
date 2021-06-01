e19cd813130d0d0a31cce5593643ef04ab315319
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2021-06-01 17:56:05 +0800

    Update dotfiles-init.Linux.sh

--- scripts/dotfiles-init.Linux.sh
+++ scripts/dotfiles-init.Linux.sh
@@ -14,7 +14,7 @@ git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME config --local status.showU
 curl -fsSL https://starship.rs/install.sh | bash -s -- -b $HOME/bin  -y
 
 git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
-$HOME/.fzf/install --all --key-bindings --completion --no-update-rc --64
+$HOME/.fzf/install --all --key-bindings --completion --no-update-rc
 
 source $HOME/.bashrc
 
