2ce90367c73d0763bcc59e65208d09ae499c1009
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2021-12-17 14:25:06 +0800

    Updated to use curl

--- scripts/setup-vim.sh
+++ scripts/setup-vim.sh
@@ -4,5 +4,5 @@
 # curl -sL https://yogendra.me/setup-vim | bash
 echo Setup VIM
 mkdir -p .vim/autoload
-wget -qO $HOME/.vim/autoload/plug.vim "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-wget -q "https://gist.githubusercontent.com/yogendra/318c09f0cd2548bdd07f592722c9bbec/raw/.vimrc" -O $HOME/.vimrc 
+curl -sSL "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" -o $HOME/.vim/autoload/plug.vim
+curl -sSL "https://raw.githubusercontent.com/yogendra/dotfiles/master/.vimrc.full" -o $HOME/.vimrc
