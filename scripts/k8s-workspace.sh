b399f133dde49062e3b5ac1786dfd22658192ba7
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2021-12-17 14:20:49 +0800

    change to curl

--- scripts/k8s-workspace.sh
+++ scripts/k8s-workspace.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-set -e 
-wget -qO- https://yogendra.me/k8s-profile >> ~/.profile
-wget -qO- https://yogendra.me/minimal-vimrc >> ~/.vimrc
+set -e
+curl -L https://yogendra.me/k8s-profile >> ~/.profile
+curl -L https://yogendra.me/minimal-vimrc >> ~/.vimrc
 
 source ~/.profile
