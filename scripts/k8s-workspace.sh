0b3359ee54dc4df7467d01adcf3a96c982205d77
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2021-12-18 09:40:28 +0800

    silent download

--- scripts/k8s-workspace.sh
+++ scripts/k8s-workspace.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -e
-curl -L https://yogendra.me/k8s-profile >> ~/.profile
-curl -L https://yogendra.me/minimal-vimrc >> ~/.vimrc
+curl -sSL https://yogendra.me/k8s-profile >> ~/.profile
+curl -sSL https://yogendra.me/minimal-vimrc >> ~/.vimrc
 
 source ~/.profile
