f6a9566e513018f1bfc7fa3aba7aaf449d0cc8fb
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-02-08 20:21:32 +0800

    Updated to work for arm64 build

--- scripts/jumpbox-init.sh
+++ scripts/jumpbox-init.sh
@@ -10,11 +10,16 @@ export PATH=${PATH}:${PROJECT_HOME}/bin
 
 TIMEZONE=${TIMEZONE:-Asia/Singapore}
 sudo ln -fs /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+
+OS="$(uname | tr '[:upper:]' '[:lower:]')"
+ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')"
+
 function setup_common(){
     echo == Common tools
 
 	OS_TOOLS=(\
 		apt-transport-https \
+        bat \
 		bzip2 \
 		ca-certificates \
 		coreutils \
@@ -31,6 +36,7 @@ function setup_common(){
 		iputils-ping \
 		iputils-tracepath \
 		jq \
+        lego \
 		less \
 		lsb-release \
 		man \
@@ -38,7 +44,7 @@ function setup_common(){
 		mtr \
 		netcat \
 		nmap \
-		python2.7-minimal \
+		python3-pip \
 		rclone \
 		screen \
 		shellcheck \
@@ -56,20 +62,12 @@ function setup_common(){
 	sudo apt-get update
 	sudo apt-get upgrade -y
 	sudo apt-get install -y "${OS_TOOLS[@]}"
-	sudo curl  -sSL https://bootstrap.pypa.io/get-pip.py | python2.7
-
-	wget -q  https://github.com/sharkdp/bat/releases/download/v0.15.4/bat-v0.15.4-x86_64-unknown-linux-gnu.tar.gz -O- | tar -C /tmp -xz bat-v0.15.4-x86_64-unknown-linux-gnu/bat
-	mv /tmp/bat-v0.15.4-x86_64-unknown-linux-gnu/bat ${PROJECT_HOME}/bin/bat
-	chmod a+x  ${PROJECT_HOME}/bin/bat
-	rm -rf /tmp/bat-v0.13.0-x86_64-unknown-linux-gnu
 
     sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CC86BB64
     sudo add-apt-repository ppa:rmescandon/yq
     sudo apt update
     sudo apt install yq -y
 
-    curl -sSL https://github.com/go-acme/lego/releases/download/v3.8.0/lego_v3.8.0_linux_amd64.tar.gz | tar -C ${PROJECT_HOME}/bin -xzv lego
-    chmod a+x ${PROJECT_HOME}/bin/lego
 }
 
 function setup_profile (){
@@ -86,7 +84,7 @@ function setup_k8s (){
 
 
 	[[ -f /etc/apt/sources.list.d/docker.list ]] ||
-        echo  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee -a /etc/apt/sources.list.d/docker.list
+        echo  "deb [arch=${ARCH}] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee -a /etc/apt/sources.list.d/docker.list
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 	[[ -f /etc/apt/sources.list.d/helm-stable-debian.list ]] ||
@@ -118,7 +116,7 @@ function setup_k8s (){
     )
 
 	export PATH="${PROJECT_HOME}/bin:${PATH}:${HOME}/.krew/bin"
-    kubectl krew install access-matrix cert-manager ctx konfig ns tail
+    kubectl krew install access-matrix cert-manager ctx konfig ns tail  || echo "[WARNING] Some plugins were not installed"
 
 	sudo usermod -aG docker $USER
 
@@ -132,91 +130,31 @@ function setup_k8s (){
 
 	helm repo update
 
-	curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-linux-amd64
+	curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-${ARCH}
 	chmod +x ./kind
 	mv ./kind ${PROJECT_HOME}/bin/kind
 
-    curl -sSL https://github.com/derailed/k9s/releases/download/v0.21.7/k9s_Linux_x86_64.tar.gz | tar -C ${PROJECT_HOME}/bin -xz k9s
-    curl -sSL  https://github.com/wagoodman/dive/releases/download/v0.9.2/dive_0.9.2_linux_amd64.tar.gz | tar -C ${PROJECT_HOME}/bin -xz dive
+    curl -sSL  https://github.com/derailed/k9s/releases/latest/download/k9s_Linux_${ARCH}.tar.gz  | tar -C ${PROJECT_HOME}/bin -xvf - k9s
+    [[ "${ARCH}" == "amd64" ]] && curl -sSL  https://github.com/wagoodman/dive/releases/download/v0.9.2/dive_0.9.2_linux_amd64.tar.gz | tar -C ${PROJECT_HOME}/bin -xz dive
 }
 
 
 function cloud_common (){
 	echo == Cloud Common
-	wget -q https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip -O /tmp/terraform.zip
-	gunzip -S .zip /tmp/terraform.zip
-	mv /tmp/terraform ${PROJECT_HOME}/bin/terraform
-	chmod a+x ${PROJECT_HOME}/bin/terraform
+    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+    sudo apt-add-repository "deb [arch=${ARCH}] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+    sudo apt-get update && sudo apt-get install terraform
 }
 
 function setup_aws (){
 	cloud_common
 	echo == AWS CLI
-	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+	[[ "${ARCH}" == "amd64 ]] && " curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    [[ "${ARCH}" == "arm64 ]] && " curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
 	unzip awscliv2.zip
 	./aws/install -i ${PROJECT_HOME}/bin/aws-cli -b ${PROJECT_HOME}/bin
 	rm -rf awscliv2.zip ./aws
 
-}
-
-function setup_tkgi(){
-
-    echo == TKGI
-    wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
-    [[ -f /etc/apt/sources.list.d/cloudfoundry-cli.list ]] ||
-        echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
-
-    # Bosh
-    URL=https://github.com/cloudfoundry/bosh-cli/releases/download/v6.3.1/bosh-cli-6.3.1-linux-amd64
-    wget -q ${URL} -O ${PROJECT_HOME}/bin/bosh
-    chmod a+x ${PROJECT_HOME}/bin/bosh
-
-    # BBR
-    URL=https://github.com/cloudfoundry-incubator/bosh-backup-and-restore/releases/download/v1.7.2/bbr-1.7.2-linux-amd64
-    wget -q ${URL} -O ${PROJECT_HOME}/bin/bbr
-    chmod a+x ${PROJECT_HOME}/bin/bbr
-
-    # Credhub
-    URL=https://github.com/cloudfoundry-incubator/credhub-cli/releases/download/2.8.0/credhub-linux-2.8.0.tgz
-    wget -q ${URL} -O- | tar -C ${PROJECT_HOME}/bin -xz  ./credhub
-    chmod a+x ${PROJECT_HOME}/bin/credhub
-
-    # Fly
-    URL=https://github.com/concourse/concourse/releases/download/v6.3.1/fly-6.3.1-linux-amd64.tgz
-    wget -q ${URL} -O- | tar -C ${PROJECT_HOME}/bin -zx fly
-    chmod a+x ${PROJECT_HOME}/bin/fly
-
-    # OM
-    URL=https://github.com/pivotal-cf/om/releases/download/6.1.2/om-linux-6.1.2.tar.gz
-    wget -q ${URL} -O- | tar -C ${PROJECT_HOME}/bin -zx om
-    chmod a+x ${PROJECT_HOME}/bin/om
-
-    # PIVNET
-    URL=https://github.com/pivotal-cf/pivnet-cli/releases/download/v1.0.4/pivnet-linux-amd64-1.0.4
-    wget -q ${URL} -O ${PROJECT_HOME}/bin/pivnet
-    chmod a+x ${PROJECT_HOME}/bin/pivnet
-
-    # UAA
-    URL=https://github.com/cloudfoundry-incubator/uaa-cli/releases/download/0.10.0/uaa-linux-amd64-0.10.0
-    wget -q ${URL} -O ${PROJECT_HOME}/bin/uaa
-    chmod a+x ${PROJECT_HOME}/bin/uaa
-
-
-    if [[ -n ${PIVNET_LEGACY_TOKEN} ]]
-    then
-        #PKS
-        VERSION=1.8.1
-        om download-product -t "${PIVNET_LEGACY_TOKEN}" -o /tmp -v "${VERSION}"  -p pivotal-container-service --pivnet-file-glob='pks-linux-amd64-*'
-        mv /tmp/pks-linux-amd64-* ${PROJECT_HOME}/bin/pks
-        chmod a+x ${PROJECT_HOME}/bin/pks
-    fi
-}
-
-function setup_vsphere (){
-
-	echo == Vsphere
-	wget -q https://github.com/vmware/govmomi/releases/download/v0.23.0/govc_linux_amd64.gz -O- | gunzip  > ${PROJECT_HOME}/bin/govc
-	chmod a+x ${PROJECT_HOME}/bin/govc
 }
 
 function setup_gcp(){
@@ -238,12 +176,6 @@ function setup_azure(){
         sudo tee /etc/apt/sources.list.d/azure-cli.list
     sudo apt-get update
     sudo apt-get install -qqy azure-cli
-}
-
-function setup_tanzu(){
-	echo == Tanzu
-	curl -sSL https://vmware.bintray.com/tmc/0.1.0-d11404fb/linux/x64/tmc -o ${PROJECT_HOME}/bin/tmc
-	chmod a+x ${PROJECT_HOME}/bin/tmc
 }
 
 for comp in $*
